31318fad53e36dee96545b271b2adc4b552e7210
Omkar Kusekar <omkarkusekar12@gmail.com>
2025-07-06 15:55:19 +0530

    application.yml file changed

--- order-service/src/test/java/com/Omkar/orderservice/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/Omkar/orderservice/OrderServiceApplicationTests.java
@@ -1,9 +1,10 @@
 package com.Omkar.order_service;
 
+import com.Omkar.orderservice.OrderServiceApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = OrderServiceApplication.class)
 class OrderServiceApplicationTests {
 
 	@Test
